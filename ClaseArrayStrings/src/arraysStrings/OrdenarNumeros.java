089e6cc3e8d1c46a841ef0f2e5e090e335d02c5c
brunoac98 <89816299+brunoac98@users.noreply.github.com>
2023-03-11 00:30:28 -0300

    Se agregan ejercicios de la Clase 4.

--- ClaseArrayStrings/src/arraysStrings/OrdenarNumeros.java
+++ ClaseArrayStrings/src/arraysStrings/OrdenarNumeros.java
@@ -5,18 +5,33 @@ public class OrdenarNumeros {
 		
 		int numeros[] = {51,2,14};		
 		int numAux = 0;
+		boolean ascen = false;
 		
 		for(int i=0;i<numeros.length - 1;i++) {
 			
 			for(int j=0;j<numeros.length - 1;j++) {
 				
-				if (numeros[i] > numeros[j + 1]) {
+				if (ascen) {
+						
+					if (numeros[i] > numeros[j + 1]) {
+						
+						numAux = numeros[i];
+						numeros[i] = numeros[j + 1];
+						numeros[j + 1] = numAux;								
+						
+						}	
+				
+					} else {
+							
+					if (numeros[i] < numeros[j + 1]) {
+						
+						numAux = numeros[i];
+						numeros[i] = numeros[j + 1];
+						numeros[j + 1] = numAux;								
+						
+						}
 					
-					numAux = numeros[i];
-					numeros[i] = numeros[j + 1];
-					numeros[j + 1] = numAux;								
-					
-				}
+					}
 				
 			}
 			
